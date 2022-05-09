b82672cd6f48f761845fde353e36ca717a5a88a2
chuckLcq <15524415221@163.com>
2022-05-09 23:16:37 +0800

    大版本升级前统一调整，填加自述文件，初始化项目调整依赖

---
+++ src/main/java/cn/structured/sa/StructureAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.structured.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructureAdminApplication.class, args);
+    }
+
+}
