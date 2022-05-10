59b9c690c374adf21f79659d9f7699147eea9b14
chuckLcq <15524415221@163.com>
2022-05-10 20:15:12 +0800

    大版本升级前统一调整，填加自述文件，初始化项目调整依赖

--- src/main/java/cn/structured/sa/StructureAdminApplication.java
+++ src/main/java/cn/structured/sa/StructureAdminApplication.java
@@ -1,8 +1,10 @@
 package cn.structured.sa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "cn.structured.sa.dao.**")
 @SpringBootApplication
 public class StructureAdminApplication {
 
